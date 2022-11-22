03c77c2160d42f4776c5bed7396592ba28339931
Steve1192 <stevesantner@gmail.com>
2022-11-21 20:23:12 -0500

    commit tree 813d40815f7a1cfd3daaf56313201c92b05a5b99

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
+var num1 = 31;
+var num2 = 2;
+
+
+var multiply = num1 * num2;
+var random = Math.ceil(Math.random() * 100);
+
+var num3 = 31;
+var num4 = 9;
+
+var mod = Math.ceil(num3 / num4);
+
+var max = (1, 2, 20);
+
+Math.max();
 // Write your code here
